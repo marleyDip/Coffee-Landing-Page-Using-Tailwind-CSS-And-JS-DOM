300ddae8fdc1b02cdb553cdc3f4ca3e7bc20916e
Md Sofian Hasan <Dip.akand9899@gmail.com>
2025-07-12 00:58:38 +0600

    Project Setup

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {},
+    fontFamily: {
+      Poppins: ["Poppins", "sans-serif"],
+    },
+  },
+  plugins: [],
+};
